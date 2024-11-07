e07327c7a5dffc7c826db657353160824e4d0880
chelangatbrigit <brigitchelangat@gmail.com>
2024-11-07 16:33:06 +0300

    Course content and accordion started

--- courses/src/components/CourseDetails.jsx
+++ courses/src/components/CourseDetails.jsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import allCourses from '../data/courses.json';
+import { KeyTakeaways } from "./NavigationData";
+import {BsClock, BsStarFill, BsPeopleFill, BsHeart, BsDash} from 'react-icons/bs';
+import { FaShare } from 'react-icons/fa';
 
 const CourseDetails = () =>{
     const location = useLocation(); 
     const { courseId } = location.state || {}; 
     const [course, setCourse] = useState(null);
+    const [courseContent, setCourseContent] = useState(null);
+    const [isActive,setIsActive] = useState(false);
     
     useEffect(() => {
-      // Assuming `courseData` is an array of courses
         const selectedCourse = allCourses.courses.find((c) => c.id === courseId);
         setCourse(selectedCourse);
     }, [courseId]);
+
+    useEffect(() => {
+        const selectedCourseContent = allCourses.content.find((c) => c.course_id === courseId);
+        setCourseContent(selectedCourseContent);
+    }, [courseId]);
+
 
     if (!course) {
         return (
@@ -25,21 +35,63 @@ const CourseDetails = () =>{
                 <h1 className=' text-base  mb-5'> <Link to={'/courses'} replace className='hover:underline hover:text-brand-light'>Courses </Link> &gt; {course.title} </h1>
                 <p className='text-base font-semibold mb-4 '>{course.domain}</p>
                 <p className='font-bold text-3xl mb-6'>{course.title}</p>
-                <p>
-                    <span>4.2</span>
-                    <span>{course.hours} Hours</span>
-                    <span>~66,350+ Enrolled Leaners</span>
+                <p className='text-[15px] font-semibold'>
+                    <BsStarFill className='inline-block mb-1.5 mr-0.5 text-yellow-300'/> <span className='font-normal mr-10'>4.2</span>
+                    <BsClock className='inline-block mb-1 mr-1' /> <span className='mr-10'>{course.hours} <span className='font-normal'>Hours</span></span>
+                    <BsPeopleFill className='inline-block mb-1 mr-1'/> <span>~{course.enrollments}+ <span className='font-normal'>Enrolled Leaners</span></span>
                 </p>
             </div>
-            <div className='grid grid-cols-6 pt-5 pl-16'>
-            <div className='course-content col-span-4 '>
-                <div className='about-course h-20 bg-white mb-4'>About Course</div>
-                <div className='course-content h-20 bg-white mb-4'>Course Content- 17 sections</div>
-                <div className='certificate h-20 bg-white mb-4'>Certificate of completion</div>
-                <div className='reviews h-20 bg-white mb-4'>Reviews</div>
+            <div className='grid grid-cols-7 pt-5 pl-2 bg-gray-100 mx-auto w-90'>
+            <div className='course-content col-span-5 '>
+                <div className='about-course bg-white mb-4 rounded-lg px-4 py-5'>
+                    <h1 className='text-gray-900 font-semibold text-lg mb-4'>About the Course</h1>
+                    <p className='text-sm text-gray-500 font-medium mb-3'>{course.about}</p>
+                </div>
+                <div className='course-content h-auto bg-white mb-4 rounded-lg py-4'>
+                    <div className="content-header pl-4 mb-3">
+                        <p className='text-lg font-semibold text-gray-900'>Course Content</p>
+                        <p className=' text-[15px] text-gray-500 font-medium mb-3'>{courseContent.modules.length} Sections </p>
+                    </div>
+                    <div className="accordion">  
+                        {
+                        courseContent.modules.map((val) => {
+                            return(
+                                <div className='accordion-item pl-4 py-4 border-t-[1px] hover:cursor-pointer' onClick={() => setIsActive(!isActive)}>
+                                    <p className='font-medium'>{val.title}</p> 
+                                </div>
+                            
+                            )
+                        })
+                    }
+             
+                    </div>
+
+                </div>
+                <div className='certificate h-20 bg-white mb-4 rounded-lg'>Certificate of completion</div>
+                <div className='reviews h-20 bg-white mb-4 rounded-lg'>Reviews</div>
             </div>
-            <div className='key-takeaways col-span-2 h-96 p-3'>
-                <div className='h-full w-full bg-white'></div>
+            <div className='col-span-2 h-full px-5'>
+                <div className='h-full w-full bg-white rounded-lg'>
+                    <div className='actions py-5 pl-10 border-b-[1px]'>
+                        <BsHeart className='inline-block mr-5 font-bold size-5'/>
+                        <FaShare className='inline-block mr-5 size-5 mb-0.5'/>
+                        <button className='bg-brand text-white py-2 px-7 font-semibold rounded-md hover:bg-brand-light hover:text-gray-800'>
+                        Enroll Now</button>
+                    </div>
+                    <div className='key-takeaways pt-4 pl-5'>
+                        <p className='font-semibold text-gray-900'>Key Takeaways</p>
+                        <ul className="list-none w-full mt-2 h-auto">
+                        {
+                        KeyTakeaways.map((val,key) => {
+                            return <li key={key} className='flex flex-row items-center gap-x-4 h-12 w-full pt-2 pr-1 pb-1 text-[15px] '>
+                                <div id='icon' ><BsDash/></div>
+                                <div id='title' >{val.title}</div>
+                            </li>
+                        })
+                    }
+                    </ul>
+                    </div>
+                </div>
             </div>
             </div>
             
--- courses/src/components/NavigationData.js
+++ courses/src/components/NavigationData.js
@@ -4,11 +4,13 @@ import { BsGrid, BsCardChecklist, BsPeopleFill, BsListUl,
 
 export const NavigationData = [
     {
+        id: 1,
         title: "Dashboard",
         icon: <BsGrid />,
         link: "/dashboard"
     },
     {
+        id: 2,
         title: "Courses",
         icon: <BsCardChecklist />,
         link: "/courses"
@@ -19,11 +21,13 @@ export const NavigationData = [
         link: "/resources"
     },
     {
+        id: 3,
         title: "Webinars",
         icon: <BsPersonWorkspace />,
         link: "/webinars"
     },
     {
+        id: 4,
         title: "Blogs",
         icon: <BsFileText />,
         link: "/blogs"
@@ -32,18 +36,59 @@ export const NavigationData = [
 
 export const CourseInfo = [
     {
+        id: 1,
         title:"learners",
         no: "45",
         icon: <BsPeopleFill />
     },
     {
+        id: 2,
         title:"lessons",
         no: "15",
         icon: <BsListUl />
     },
     {
+        id: 3,
         title:"rating",
         no:"4.5",
+        icon: <BsFillStarFill />
+    }
+]
+
+export const KeyTakeaways = [
+    {
+        id: 1,
+        title:"Learn from Industry Leaders",
+        icon: <BsFillStarFill />
+    },
+    {
+        id: 2,
+        title:"Industry aligned Projects",
+        icon: <BsFillStarFill />
+    },
+    {
+        id: 3,
+        title:"Certification",
+        icon: <BsFillStarFill />
+    },
+    {
+        id: 4,
+        title:"Personalized Career Coaching",
+        icon: <BsFillStarFill />
+    },
+    {
+        id: 5,
+        title:"Case Study-Based Learning",
+        icon: <BsFillStarFill />
+    },
+    {
+        id: 6,
+        title:"Doubt Clearing",
+        icon: <BsFillStarFill />
+    },
+    {
+        id: 7,
+        title:"Practical Hands-On Exercises",
         icon: <BsFillStarFill />
     }
 ]
